6f52f7a04bc8684455ceb84f62ae48ed401eadc4
iamfelipes <flipe.silva@terra.com.br>
2024-01-20 00:16:17 -0300

    id pokemon fixed

--- js/poke-api.js
+++ js/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon();
-    pokemon.number = pokeDetail.order;
+    pokemon.number = pokeDetail.id;
     pokemon.name = pokeDetail.name;
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
@@ -25,7 +25,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
 }
 
 
-pokeApi.getPokemons = (offset = 0, limit = 20 ) =>{
+pokeApi.getPokemons = ( offset = 0, limit = 0 ) =>{
     const url = `https://pokeapi.co/api/v2/pokemon?&offset=${offset}&limit=${limit}`;
     return fetch(url)
     .then((response) => response.json())
