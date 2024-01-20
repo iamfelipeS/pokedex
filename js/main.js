3f36066950c086b5792dc67050ad66082da2a12c
iamfelipes <flipe.silva@terra.com.br>
2024-01-19 23:26:04 -0300

    pokemon Model and convert PokeApi to new DetailPokemon

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-
-function convertPokemnonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`).join('')
-}
-
-function convertPokemnonToLi(pokemon) {
-    return ` 
-        <li class="pokemon">
-            <span class="number">"#${pokemon.order}"</span>
-            <span class="name">${pokemon.name}</span>
-        
-            <div class="details">
-                <ol id="pokemonList" class="types">
-                   ${convertPokemnonTypesToLi(pokemon.types)}
+function convertPokemonToLi(pokemon) {
+        return ` 
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
-                 <img src="${pokemon.sprites.front_default}"
-                    alt="${pokemon.name}">
+                
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -23,8 +18,8 @@ function convertPokemnonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons = []) =>  {
-   const newHtml = pokemons.map(convertPokemnonToLi).join('')
-   pokemonList.innerHTML = newHtml
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
-    
+
